150612c711179e4f8052bc43b4370f3e968a61f8
hydrater <liquid_assassin@hotmail.com>
2015-09-15 07:14:55 +0800

    Save system up and file organising + bgm test

---
+++ TheLastHope/Assets/Programming Utilities/Save System/Save Variables/AdvancedSaveSystem.cs
@@ -0,0 +1,110 @@
+﻿//Advanced Save System - nDev Studios
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System;
+using System.Security.Cryptography;
+public class AdvancedSaveSystem : MonoBehaviour {
+
+	//Public variables
+    public string specialPath = "%appdata%"; //Use : %SystemFolderName%
+    public string folderName = "TLH";
+    public string[] variablesName;
+    public string[] variablesValue;
+    //
+    static string PasswordHash;
+    static string SaltKey;
+    static string VIKey;
+    public void SaveData(int slot)
+    {
+        try
+        {
+
+            string path = Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName + "\\Slot" + slot.ToString();
+            for (int i = 0; i < variablesName.Length; )
+            {
+                string unE = variablesValue[i];
+
+                File.WriteAllText(path + "\\" + variablesName[i], Encrypt(unE));
+                AdvancedSaveSystem_FileEncryptor.EncryptFile(path + "\\" + variablesName[i], path + "\\" + variablesName[i] + ".hope");
+                i++;
+            }
+        }
+        catch 
+        { 
+        }
+    }
+
+    public void LoadData(int slot)
+    {
+        string path = Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName + "\\Slot" + slot.ToString();
+          for (int i = 0; i < variablesName.Length; )
+          {
+			AdvancedSaveSystem_FileEncryptor.DecryptFile(path + "\\" + variablesName[i] + ".hope", (path + "\\" + variablesName[i]));
+              string toD = File.ReadAllText(path + "\\" + variablesName[i]);
+              variablesValue[i] = Decrypt(toD);
+              File.Delete(path + "\\" + variablesName[i]);
+              i++;
+          }
+    }
+    void Start()
+    {
+        PasswordHash = transform.GetComponent<AdvancedSaveSystem_EncryptDetails>().PasswordHash;
+        SaltKey = transform.GetComponent<AdvancedSaveSystem_EncryptDetails>().SaltKey;
+        VIKey = transform.GetComponent<AdvancedSaveSystem_EncryptDetails>().VIKey;
+        try
+        {
+            Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName);
+            Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName + "\\Slot1");
+            Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName + "\\Slot2");
+            Directory.CreateDirectory(Environment.ExpandEnvironmentVariables(specialPath) + "\\" + folderName + "\\Slot3");
+        }
+        catch 
+        { 
+        }
+    }
+    void Update()
+    {
+       
+    }
+    public static string Encrypt(string plainText)
+    {
+        byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+        byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+        var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+        var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+
+        byte[] cipherTextBytes;
+
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                cipherTextBytes = memoryStream.ToArray();
+                cryptoStream.Close();
+            }
+            memoryStream.Close();
+        }
+        return Convert.ToBase64String(cipherTextBytes);
+    }
+    public static string Decrypt(string encryptedText)
+    {
+        byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+        byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+        var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+        var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+        var memoryStream = new MemoryStream(cipherTextBytes);
+        var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+        int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+        memoryStream.Close();
+        cryptoStream.Close();
+        return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+    }
+}
---
+++ TheLastHope/Assets/Scripts/mainMenuUI.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mainMenuUI : MonoBehaviour {
+	[SerializeField] GameObject start, createnew, options, exit;
+	bool hoverStart, hoverNew, hoverOption, hoverExit;
+
+	void Start()
+	{
+		hoverStart = hoverNew = hoverOption = hoverExit = false;
+	}
+
+	void Update()
+	{
+		//Update the alpha of the buttons
+		//Start
+		if (hoverStart) {
+			if (start.GetComponent<Image> ().color.a < 1) {
+				start.GetComponent<Image> ().color = new Color (255, 255, 255, start.GetComponent<Image> ().color.a + Time.deltaTime);
+			}
+		} else {
+			if(start.GetComponent<Image>().color.a > 0.745){
+				start.GetComponent<Image> ().color = new Color (255, 255, 255, start.GetComponent<Image> ().color.a - Time.deltaTime);
+			}
+		}
+		//New
+		if (hoverNew) {
+			if (createnew.GetComponent<Image> ().color.a < 1) {
+				createnew.GetComponent<Image> ().color = new Color (255, 255, 255, createnew.GetComponent<Image> ().color.a + Time.deltaTime);
+			}
+		} else {
+			if(createnew.GetComponent<Image>().color.a > 0.745){
+				createnew.GetComponent<Image> ().color = new Color (255, 255, 255, createnew.GetComponent<Image> ().color.a - Time.deltaTime);
+			}
+		}
+		//Option
+		if (hoverOption) {
+			if (options.GetComponent<Image> ().color.a < 1) {
+				options.GetComponent<Image> ().color = new Color (255, 255, 255, options.GetComponent<Image> ().color.a + Time.deltaTime);
+			}
+		} else {
+			if(options.GetComponent<Image>().color.a > 0.745){
+				options.GetComponent<Image> ().color = new Color (255, 255, 255, options.GetComponent<Image> ().color.a - Time.deltaTime);
+			}
+		}
+		//Exit
+		if (hoverExit) {
+			if (exit.GetComponent<Image> ().color.a < 1) {
+				exit.GetComponent<Image> ().color = new Color (255, 255, 255, exit.GetComponent<Image> ().color.a + Time.deltaTime);
+			}
+		} else {
+			if(exit.GetComponent<Image>().color.a > 0.745){
+				exit.GetComponent<Image> ().color = new Color (255, 255, 255, exit.GetComponent<Image> ().color.a - Time.deltaTime);
+			}
+		}
+	}
+
+	//Hover over start
+	public void HoverOverStart()
+	{
+		hoverStart = true;
+	}
+
+	public void HoverOutStart()
+	{
+		hoverStart = false;
+	}
+
+	//Hover over create new
+	public void HoverOverNew()
+	{
+		hoverNew = true;
+	}
+	
+	public void HoverOutNew()
+	{
+		hoverNew = false;
+	}
+
+	//Hover over option
+	public void HoverOverOpt()
+	{
+		hoverOption = true;
+	}
+	
+	public void HoverOutOpt()
+	{
+		hoverOption = false;
+	}
+
+	//Hover over exit
+	public void HoverOverExit()
+	{
+		hoverExit = true;
+	}
+	
+	public void HoverOutExit()
+	{
+		hoverExit = false;
+	}
+
+	// Use this for initialization
+	void OnEnable() {
+		Application.Quit();
+		Debug.Log("Game closed");
+	}
+
+}
