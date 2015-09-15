d63254a34f7fc402b4930f6a0550a1ec817699b0
hydrater <liquid_assassin@hotmail.com>
2015-09-15 23:03:36 +0800

    Video and other

---
+++ TheLastHope/Assets/__mainMenu/Movie.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movie : MonoBehaviour {
+	[SerializeField] MovieTexture movTexture;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		GetComponent<AudioSource>().clip = movTexture.audioClip;
+		movTexture.Play();
+		GetComponent<AudioSource>().Play();
+	}
+	
+}
