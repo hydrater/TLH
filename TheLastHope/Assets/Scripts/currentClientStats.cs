63474b476949d617fc4ec6f9f645688128fce84d
hydrater <liquid_assassin@hotmail.com>
2015-09-18 17:38:52 +0800

    Map Selection half done

--- TheLastHope/Assets/Scripts/currentClientStats.cs
+++ TheLastHope/Assets/Scripts/currentClientStats.cs
@@ -2,6 +2,11 @@
 using System.Collections;
 
 public class currentClientStats : MonoBehaviour {
-	public string playerName, roomName, password;
-	public byte charNo;
+	public string playerName, roomName, password, time;
+	public byte charNo, lastDiff, level;
+	
+	void Update()
+	{
+		time = System.DateTime.Now.ToString("HH,mm,dd,MM");
+	}
 }
---
+++ TheLastHope/Assets/_mapSelection/mapSelectionUI.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class mapSelectionUI : Photon.MonoBehaviour {
+	byte diff = 0, levelSelected = 0;
+	public byte maxLevelAmount;
+	public GameObject mapText, diffText;
+	private currentClientStats gameStat;
+	
+	void Start()
+	{
+		maxLevelAmount -= 1;
+		gameStat = GameObject.Find("GameManager").GetComponent<currentClientStats>();
+	}
+	
+	public void levelLeft()	
+	{
+		if (levelSelected == 0)
+			levelSelected = maxLevelAmount;
+		else
+		--levelSelected;
+		
+		gameStat.level = levelSelected;
+	}
+	
+	public void levelRight()	
+	{
+		if (levelSelected == maxLevelAmount)
+			levelSelected = 0;
+		else
+			++levelSelected;
+			
+		gameStat.level = levelSelected;
+	}
+	
+	public void diffLeft()	
+	{
+		if (diff == 0)
+			diff = 3;
+		else
+		--diff;
+		
+		gameStat.lastDiff = diff;
+	}
+	
+	public void diffRight()	
+	{
+		if (diff == 3)
+			diff = 0;
+		else
+		++diff;
+		
+		gameStat.lastDiff = diff;
+	}
+}
