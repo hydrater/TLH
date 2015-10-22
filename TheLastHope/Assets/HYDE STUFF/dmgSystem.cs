fc861804caec0486d48b863f825b942e434d44f6
CykaBylat <hydroidprime@gmail.com>
2015-10-22 12:50:03 +0800

    Raw code 2.0

---
+++ TheLastHope/Assets/HYDE STUFF/dmgSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dmgSystem : MonoBehaviour {
+	public float HP = 100f;
+	public float Armor = 5;
+	private float DmgTaken;
+	public float GunDmg;
+	
+	//	void OnCollision (Collision col){
+	//		if (col.gameObject.name == ("Bullet"))
+	//		{
+	//			DmgTaken = GunDmg - (Armor / 4);
+	//		}
+	//        if (HP <= 0)
+	//        {
+	//            Destroy(gameObject);
+	//        }
+	//	}
+}
