fcff4aaebc21cae3fad1f7a69edfe2f820ff35b0
hydrater <liquid_assassin@hotmail.com>
2015-10-29 23:39:28 +0800

    Sanc layout and desert texture and weapon scripts set up

---
+++ TheLastHope/Assets/Weapons/Weapon Scripts/w1001.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class w1001 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
