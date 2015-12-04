9661b7c654a2bc8614fc3d3077becd3f0355243e
hydrater <liquid_assassin@hotmail.com>
2015-12-04 20:16:51 +0800

    dayNight done

--- TheLastHope/Assets/Scripts/_general/dayNight.cs
+++ TheLastHope/Assets/Scripts/_general/dayNight.cs
@@ -25,35 +25,52 @@ public class dayNight : MonoBehaviour {
 			switch(fadeType)
 			{
 			case 0:
-				if (RenderSettings.skybox.GetColor("_Tint").r * 255 > 30)
+				if (RenderSettings.skybox.GetColor("_Tint").r * 255 > 18)
 				{
-					RenderSettings.skybox.SetColor( "_Tint", new Color (RenderSettings.skybox.GetColor("_Tint").r - Time.deltaTime * 0.01f, 
-					                                                    RenderSettings.skybox.GetColor("_Tint").g - Time.deltaTime * 0.01f, 
+					RenderSettings.skybox.SetColor( "_Tint", new Color (RenderSettings.skybox.GetColor("_Tint").r - Time.deltaTime * 0.0017f, 
+					                                                    RenderSettings.skybox.GetColor("_Tint").g - Time.deltaTime * 0.0017f, 
 					                                                    RenderSettings.skybox.GetColor("_Tint").b));
 				}
-				else
-				{
-					//RenderSettings.skybox = skies[3];
-				}
 				
-				if (RenderSettings.skybox.GetColor("_Tint").b * 255 > 60)
+				if (RenderSettings.skybox.GetColor("_Tint").b * 255 > 40)
 				{
 					RenderSettings.skybox.SetColor( "_Tint", new Color (RenderSettings.skybox.GetColor("_Tint").r , 
 					                                                    RenderSettings.skybox.GetColor("_Tint").g , 
-					                                                    RenderSettings.skybox.GetColor("_Tint").b - Time.deltaTime * 0.01f));
+					                                                    RenderSettings.skybox.GetColor("_Tint").b - Time.deltaTime * 0.0014f));
 				}
 				
+				if (RenderSettings.ambientLight.r * 255 > 215)
+				{
+					RenderSettings.ambientLight = new Color (RenderSettings.ambientLight.r - Time.deltaTime * 0.0017f,
+					                                         RenderSettings.ambientLight.g - Time.deltaTime * 0.0017f,
+					                                         RenderSettings.ambientLight.b);
+				}
 				
-				if (RenderSettings.ambientIntensity > 0.25f)
-					RenderSettings.ambientIntensity -= Time.deltaTime * 0.008f;
-				else
-					RenderSettings.ambientLight = Color.blue;
+				if (RenderSettings.ambientIntensity > 0.31f)
+				{
 					
-				if (Sun.GetComponent<Light>().intensity > 0.3f)
-					Sun.GetComponent<Light>().intensity -= Time.deltaTime * 0.02f;
-				else
-					Sun.GetComponent<Light>().color = Color.blue;
+					RenderSettings.ambientIntensity -= Time.deltaTime * 0.0008f;
+				}
 				
+				if (Sun.GetComponent<Light>().color.r * 255 > 40)
+				{
+					Sun.GetComponent<Light>().color = new Color(Sun.GetComponent<Light>().color.r  - Time.deltaTime * 0.0017f,
+					                                            Sun.GetComponent<Light>().color.g  - Time.deltaTime * 0.0017f,
+					                                            Sun.GetComponent<Light>().color.b);
+				}
+				
+				if (Sun.GetComponent<Light>().color.b * 255 > 110)
+				{
+					Sun.GetComponent<Light>().color = new Color(Sun.GetComponent<Light>().color.r,
+					                                            Sun.GetComponent<Light>().color.g,
+					                                            Sun.GetComponent<Light>().color.b - Time.deltaTime * 0.0017f);
+				}
+					
+				if (Sun.GetComponent<Light>().intensity > 0.8f)
+				{
+					
+					Sun.GetComponent<Light>().intensity -= Time.deltaTime * 0.0017f;
+				}
 				break;
 				
 			case 1:
