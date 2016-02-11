5cdb45c37e2dd2a5aaeac8f2e883f90db8d688d5
hydrater <liquid_assassin@hotmail.com>
2016-02-11 15:18:35 +0800

    Qian yu where yo work

---
+++ TheLastHope/Assets/Scripts/Weapon/Grenades.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grenades : Photon.MonoBehaviour {
+	Vector3 realPosition = Vector3.zero;
+	Quaternion realRotation = Quaternion.identity;
+	float timer = 3;
+	public string explosion;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		if (!photonView.isMine)
+		{
+			Destroy(GetComponent<Rigidbody>());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (photonView.isMine)
+		{
+			timer -= Time.deltaTime;
+			if (timer <= 0)
+			{
+				PhotonNetwork.Instantiate(explosion, transform.position, Quaternion.identity, 0);
+			}
+		}
+		else
+		{
+			transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+		}
+	}
+	
+	public void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+			
+		}
+		else
+		{
+			realPosition = (Vector3)stream.ReceiveNext();
+			realRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
