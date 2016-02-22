5e945c7c1aa32907d5bb5706f1560e7d1d314845
ChinChengChua <triplec51@hotmail.com>
2016-02-22 17:59:32 +0800

    K

--- TheLastHope/Assets/Scripts/Combat/combatStats.cs
+++ TheLastHope/Assets/Scripts/Combat/combatStats.cs
@@ -6,6 +6,7 @@ public class combatStats : Photon.MonoBehaviour {
 	public float hp = 100, hpM = 100, stam = 100, stamM = 100, walkSpeed = 5, runSpeed = 10, crouchSpeed = 2.5f;
 	public float stamCD = -1;
 	public getrekt ragDoll, rektHuD;
+	bool isDead = false;
 
 	void Start()
 	{
@@ -28,13 +29,16 @@ public class combatStats : Photon.MonoBehaviour {
 		{
 			if (photonView.isMine)
 			{
-				++GetComponent<combatHandler>().gameStat.death;
-				GameObject.Find("GameManager").GetComponent<generalNetworking>().Respawn();
-				GameObject temp = GameObject.Find("RespawnScreen");
-				temp.SetActive(true);
-				temp.GetComponent<respawnUI>().startTimer();
-				PhotonNetwork.Destroy(this.gameObject);
-				this.enabled = false;
+				if (!isDead)
+				{
+					++GetComponent<combatHandler>().gameStat.death;
+					GameObject.Find("GameManager").GetComponent<generalNetworking>().Respawn();
+					GameObject temp = GameObject.Find("RespawnScreen");
+					temp.SetActive(true);
+					temp.GetComponent<respawnUI>().startTimer();
+					PhotonNetwork.Destroy(this.gameObject);
+					isDead = true;
+				}
 			}
 			Instantiate(ragDoll, transform.position,Quaternion.identity);
 		}
